88e2e82dab4fba0b5a832076659c5834b6614927
pitisalcedo <66760644+pitisalcedo@users.noreply.github.com>
2020-07-20 16:22:59 -0500

    Updated and cleaned Search City functions
    Cleaned up search city function
    Added handleSubmit function
    Called searchCity function to display Chicago as default

--- src/index.js
+++ src/index.js
@@ -42,19 +42,20 @@ function formatDate(date) {
   return `${day}, ${months} ${todayDate}, ${year} | ${hours} : ${minutes}`;
 }
 
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-search");
-  let cityDisplay = document.querySelector("h1");
-  cityDisplay.innerHTML = `${city.value}`;
-
-  let fiveDayDisplay = document.querySelector("#selected-city");
-  fiveDayDisplay.innerHTML = `${city.value}`;
-
+function searchCity(city) {
   let apiKey = "87674b91999873f45e8ffe2337e0d7e3";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
   axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-search").value;
+
+  document.querySelector("#selected-city").innerHTML = "#city-search".value;
+
+  searchCity(city);
 }
 
 function showTemperature(response) {
@@ -98,10 +99,12 @@ let currentDate = new Date();
 dateElement.innerHTML = formatDate(currentDate);
 
 let searchForm = document.querySelector("#search-city-form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", handleSubmit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
 
 let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentPosition);
+
+searchCity("Chicago");
